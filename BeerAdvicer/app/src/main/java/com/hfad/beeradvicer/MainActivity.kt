588812579e28d171e58048755d3039d5374f591d
Daniil Polyakov <polyakovd194@gmail.com>
2024-10-19 15:15:18 +0700

    Добавление метода getBeers(), выполняющего функцию вывода марок пива по передаваемому в качестве параметра виду

--- BeerAdvicer/app/src/main/java/com/hfad/beeradvicer/MainActivity.kt
+++ BeerAdvicer/app/src/main/java/com/hfad/beeradvicer/MainActivity.kt
@@ -5,20 +5,31 @@ import android.widget.Button
 import android.widget.Spinner
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.hfad.beeradvicer.R
 
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val findBeer = findViewById<Button>(R.id.find_beer)
         findBeer.setOnClickListener {
             val beerColor = findViewById<Spinner>(R.id.beer_color)
             val color = beerColor.selectedItem
+            val beerList = getBeers(color.toString())
+            val beers = beerList.reduce { str, item -> str + '\n' + item }
             val brands = findViewById<TextView>(R.id.brands)
-            brands.text = "Beer color is $color"
+            brands.text = beers
         }
     }
 }
+    fun getBeers(color: String): List<String> {
+        return when (color)
+        {
+            "Light" -> listOf("Jail Pale Ale", "Lager Lite")
+            "Amber" -> listOf("Jack Amber", "Red Moose")
+            "Brown" -> listOf("Brown Bear Beer", "Bock Brownie")
+            else -> listOf("Gout Stout", "Dark Daniel")
+        }
+    }
