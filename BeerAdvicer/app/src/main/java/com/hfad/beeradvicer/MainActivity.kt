16a1fb2b3b3f238ad0c133c31f7086aa24f18f31
Daniil Polyakov <polyakovd194@gmail.com>
2024-10-19 15:11:12 +0700

    Обновление кода файла описания логики работы приложения MainActivity.kt

--- BeerAdvicer/app/src/main/java/com/hfad/beeradvicer/MainActivity.kt
+++ BeerAdvicer/app/src/main/java/com/hfad/beeradvicer/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.hfad.beeradvicer
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.hfad.beeradvicer.R
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val findBeer = findViewById<Button>(R.id.find_beer)
+        findBeer.setOnClickListener {
+            val beerColor = findViewById<Spinner>(R.id.beer_color)
+            val color = beerColor.selectedItem
+            val brands = findViewById<TextView>(R.id.brands)
+            brands.text = "Beer color is $color"
         }
     }
 }
